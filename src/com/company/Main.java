2dd009337ae9df4950a1494ad7b8ae0810fbac36
nilssondennis <nilsson.dennis@hotmail.com>
2021-01-07 11:25:24 +0100

    First upload of Calculator project.

---
+++ src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+        String userMenuInput = "";
+        int userInt1 = 0;
+        int userInt2 = 0;
+        String userMethod = "";
+        int sum = 0;
+
+        // While
+        while(!(userMenuInput.equals("4"))){
+        // Meny
+        System.out.println("Miniräknare");
+        System.out.println("Välj ett av följande alternativ\n");
+
+        System.out.println("1: Välj ett räknesätt: + eller - eller * eller /");
+        System.out.println("2: Mata in två heltal");
+        System.out.println("3. Gör uträkningen och skriv resultatet");
+        System.out.println("4. Avsluta programmet");
+        userMenuInput = myScanner.nextLine();
+
+        // "Clear" the terminal (by writing 50 linebreaks)
+            System.out.println("\n".repeat(60));
+
+        switch(userMenuInput){
+            case "1":
+                System.out.println("Choose a method");
+                userMethod = myScanner.nextLine();
+                break;
+            case "2":
+                System.out.println("Please input 2 numbers");
+                userInt1 = myScanner.nextInt();
+                userInt2 = myScanner.nextInt();
+                System.out.println("Thanks, you have chosen numbers: " + userInt1 + " " + userInt2);
+                break;
+            case "3":
+                if(userMethod.equals("+")){
+                    sum = userInt1 + userInt2;
+                }else if(userMethod.equals("-")){
+                    sum = userInt1 - userInt2;
+                }else if (userMethod.equals("*")){
+                    sum = userInt1 * userInt2;
+                }else if (userMethod.equals("/")){
+                    sum = userInt1 / userInt2;
+                }
+                System.out.println("The total sum is: " + sum + "\n");
+                break;
+        }
+
+        }
+
+
+    }
+}
